a12895979a06a067222177fd84de71cbf551d632
Yang <yang.wang04@gmail.com>
2016-08-25 14:21:13 +1000

    Use jQuery append function to add element into index page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ // $("#main").append("Yang");
+
+var firstName = "james";
+var age = 32;
+console.log(firstName);
+
+var email = "cameron@udacity.com";
+
+var newEmail = email.replace("udacity","gmail");
+
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = "I am Yang and I am AWESOME!";
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+$("#main").append(funThoughts);
+
+console.log(funThoughts);
